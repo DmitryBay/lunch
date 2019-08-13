6a1213c211e778ef8a2813e6c46e8175e5d011bb
dmitry <baydmitry@gmail.com>
2019-08-13 18:28:45 +0700

    mailer ZONE EU update

--- common/config/main.php
+++ common/config/main.php
@@ -14,11 +14,14 @@ return [
         ],
         'mailer' => [
             //'class' => 'yii\swiftmailer\Mailer',
-            'class' => \YarCode\Yii2\Mailgun\Mailer::class,
+//            'class' => \YarCode\Yii2\Mailgun\Mailer::class,
+            'class' =>\common\components\Mailer::class,
+            'apiEndpoint' => 'api.eu.mailgun.net',
+//            'apiEndpoint' => getenv('MAILGUN_DOMAIN'),
             'domain' => getenv('MAILGUN_DOMAIN'),
             'apiKey' => getenv('MAILGUN_API_KEY'),
             'useFileTransport' => filter_var(getenv('MAILER_USE_FILE_TRANSPORT'), FILTER_VALIDATE_BOOLEAN),
-            'viewPath' => '@app/mail',
+            'viewPath' => '@common/mail',
             //   'textLayout' => 'my/layout',  // custome layout
             'htmlLayout' => 'layouts/html', // disable layout
             //        'apiKey' => getenv('SENDGRID_API_KEY'),
